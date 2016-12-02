e595b0d534bf38d49ad6a62e470d5512ea3668e6
zen chen <chenzenian@live.com>
2016-12-02 10:52:06 +0800

    commit

--- src/main/java/webDatabase/model/Tagm.java
+++ src/main/java/webDatabase/model/Tagm.java
@@ -1,10 +1,17 @@
 package webDatabase.model;
 
+import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.websocket.Session;
+
+//import com.mysql.jdbc.Statement;
+
+import webDatabase.database.DBConnection;
 import webDatabase.database.DatabaseHelper;
 import webDatabase.database.DatabaseResult;
 
@@ -21,15 +28,19 @@ public class Tagm {
 			return false;
 		}
 	}
-	/*
+	
 	public static boolean deleteTag(String tagName){
 		try {
+			Connection connection = DBConnection.getConnection();
+			Statement statement = connection.createStatement();
 			
+			statement.close();
+			connection.close();
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
 	}
-	*/
+	
 	public static List<Map<String,Object>> getArticleTag(String aid)throws SQLException{
 		//List<String> list;
 		DatabaseResult result =DatabaseHelper.query("select * from articletag join tag on articletag.tagId=tag.id where articletag.articleId=?", aid);
